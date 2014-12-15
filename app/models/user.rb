1ed0cfcacdc00f15d46c9aa80379166d5f050946
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-16 01:59:38 +0900

    @@ignore add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: users # ユーザ
+#
+#  id         :integer          not null, primary key # ID
+#  name       :string(255)      not null              # ユーザ名
+#  created_at :datetime         not null              # 作成日時
+#  updated_at :datetime         not null              # 更新日時
+#
+
 class User < ActiveRecord::Base
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users # ユーザ
+#
+#  id         :integer          not null, primary key # ID
+#  name       :string(255)      not null              # ユーザ名
+#  created_at :datetime         not null              # 作成日時
+#  updated_at :datetime         not null              # 更新日時
+#
+
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
