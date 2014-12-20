24613f301c7acf6f5e52275b4cc7966e0a03a488
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-20 20:10:41 +0900

    @@ignore add

--- config/initializers/simple_form_cascade.rb
+++ config/initializers/simple_form_cascade.rb
@@ -15,11 +15,15 @@ SimpleForm.setup do |config|
 
     c.wrapper tag: 'div', class: 'col width-fill' do |cc|
       cc.wrapper tag: 'div', class: 'cell' do |ccc|
-        ccc.use :input, wrap_with: { tag: 'div', class: 'left-input' }
+        ccc.wrapper tag: 'div', class: 'col width-3of4' do |cccc|
+          cccc.use :input, wrap_with: { tag: 'div', class: 'cell left-input' }
+        end
 
-        ccc.wrapper tag: 'div', class: 'col' do |cccc|
-          cccc.wrapper tag: 'ul', class: 'parsley-error-list' do |ccccc|
-            ccccc.use :error, wrap_with: { tag: 'li', class: 'custom-error-message' }
+        ccc.wrapper tag: 'div', class: 'col width-fill' do |cccc|
+          cccc.wrapper tag: 'div', class: 'cell' do |ccccc|
+            ccccc.wrapper tag: 'ul', class: 'parsley-error-list' do |cccccc|
+              cccccc.use :error, wrap_with: { tag: 'li', class: 'custom-error-message' }
+            end
           end
         end
       end
