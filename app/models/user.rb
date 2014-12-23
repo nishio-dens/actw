5de62469f681bd20cd060804dc09cd2d269f30d7
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-23 18:53:03 +0900

    @@ignore add

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@
 #
 
 class User < ActiveRecord::Base
+    devise :database_authenticatable, :omniauthable, :recoverable,
+           :registerable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'mypages#index'
 
+  devise_for :users, controllers: {
+    sessions:      "users/sessions",
+    registrations: "users/registrations",
+    passwords:     "users/passwords",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   resources :users, only: [:index]
 
   resources :mypages, only: [:index]
