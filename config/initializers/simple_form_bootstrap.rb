1c06fd4dcee38b118df0d617486ea3bad4be44fd
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-23 18:06:37 +0900

    @@ignore add

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -58,13 +58,29 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label, class: 'col-sm-3 control-label'
+    b.use :label, class: 'col-sm-2 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+    b.wrapper tag: 'div', class: 'col-sm-10' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
+  end
+
+  config.wrappers :checkbox, tag: :div, class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+
+    b.wrapper tag: 'div', class: 'col-sm-offset-2 col-sm-10'  do |ba|
+      ba.wrapper tag: 'div', class: 'checkbox' do |baa|
+        baa.wrapper tag: :label do |baaa|
+          baaa.use :input
+          baaa.use :label_text
+        end
+      end
+    end
+
+    b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+    b.use :error, wrap_with: { tag: :span, class: "help-block text-danger" }
   end
 
   config.wrappers :horizontal_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
