22f682592d79b9cb1542eb1e453a6b34862ec548
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-23 18:44:00 +0900

    @@ignore add

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@
 #
 #  id         :integer          not null, primary key # ID
 #  name       :string(255)      not null              # ユーザ名
+#  uid        :string(255)      default(""), not null # UID
+#  provider   :string(255)      default(""), not null # プロバイダ
 #  created_at :datetime         not null              # 作成日時
 #  updated_at :datetime         not null              # 更新日時
 #
