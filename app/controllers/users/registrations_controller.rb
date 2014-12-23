2d34bd131268c029e33a98d81ddd05f5b91754ea
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-23 19:30:22 +0900

    @@ignore add

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = SecureRandom.uuid
     super
   end
+
+  def create
+    @user = User.new(registration_params)
+    if @user.valid?
+      @user.save
+      render 'finish'
+    else
+      super
+    end
+  end
+
+  def confirm
+    @address = confirm_params
+  end
+
+  private
+
+  def registration_params
+    params
+      .require(:user)
+      .permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def confirm_params
+    params.permit(:address)
+  end
 end
