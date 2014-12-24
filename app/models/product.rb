58a87536334ef3579859ad09bcb60b4b4de39522
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-25 01:58:06 +0900

    @@ignore add

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: products # 成果物
+#
+#  id           :integer          not null, primary key # ID
+#  title        :string(255)      not null              # タイトル
+#  description  :text                                   # 概要
+#  url          :text             not null              # URL
+#  user_id      :integer          not null              # ユーザID
+#  published_at :datetime         not null              # 公開日
+#  created_at   :datetime         not null              # 作成日時
+#  updated_at   :datetime         not null              # 更新日時
+#
+
+class Product < ActiveRecord::Base
+end
