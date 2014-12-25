044771b46822b219daa9a412425e9692e1c16b2c
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-26 01:21:37 +0900

    @@ignore add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require jquery-ui/sortable
 //= require twitter/bootstrap
 //= require sortable
-//= require tag-it
+//= require vendor/assets/javascripts/
 //= require common
 //= require_tree .
