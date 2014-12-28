fe58464b199576f98b587652183f1db5693e6ab0
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-29 03:06:38 +0900

    @@ignore add

--- app/controllers/api/v1/filters_controller.rb
+++ app/controllers/api/v1/filters_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::ProductsController < Api::V1::BaseController
-  def index
+class Api::V1::FiltersController < Api::V1::BaseController
+  def show
     @products = Product
       .joins(:product_filters)
       .joins(:filters)
