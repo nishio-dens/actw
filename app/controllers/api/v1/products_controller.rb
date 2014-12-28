4643d5b94557be0f3ef62be529634027e79058f1
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-29 02:26:11 +0900

    @@ignore add

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,3 +1,11 @@
-class Api::V1::ProductsController < ApplicationController
+class Api::V1::ProductsController < Api::V1::BaseController
   renpond_to :json
+
+  before_action :authenticate_user!
+
+  def index
+    @products = Product
+      .where(user_id: current_user.id)
+      .order(published_at: :desc)
+  end
 end
