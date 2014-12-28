2c4a3738edee1bd651b32e9eb150d24b34d6bc88
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-29 02:39:29 +0900

    @@ignore add

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::ProductsController < Api::V1::BaseController
-  before_action :authenticate_user!
-
   def index
     @products = Product
-      .where(user_id: current_user.id)
+      .where(user_id: params[:user_id])
       .order(published_at: :desc)
   end
 end
--- app/views/api/v1/products/index.json.rabl
+++ app/views/api/v1/products/index.json.rabl
@@ -1,1 +1,2 @@
 collection @products
+attributes :id, :title, :url, :description, :category_id, :user_id, :published_at
