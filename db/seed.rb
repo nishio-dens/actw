fc36fa296b77bf0d8f4aef45b3ab40a6a5e75885
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 01:30:16 +0900

    @@ignore add

--- db/seed.rb
+++ db/seed.rb
@@ -3,10 +3,12 @@ connection = ActiveRecord::Base.connection
 tables = %w[categories]
 
 tables.each do |table|
+  Rails.logger.log("TRUNCATE TABLE #{table}")
   connection.execute("TRUNCATE #{table}")
 end
 
 Dir.glob("db/seed_sql/*.sql").each do |sql_file|
+  Rails.logger.log("load data #{sql_file}")
   sql = File.read(sql_file)
   statements = sql.split(/;$/)
   statements.pop
