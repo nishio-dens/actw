16188c0836230b2b514a05e3647cea382a50752e
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 01:48:10 +0900

    @@ignore add

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,11 @@
 #
 # Table name: categories # カテゴリ
 #
-#  id         :integer          not null, primary key # ID
-#  name       :string(255)      not null              # カテゴリ名
-#  created_at :datetime         not null              # 作成日時
-#  updated_at :datetime         not null              # 更新日時
+#  id         :integer          not null, primary key  # ID
+#  name       :string(255)      not null               # カテゴリ名
+#  selectable :boolean          default("1"), not null # ユーザが選択可能
+#  created_at :datetime         not null               # 作成日時
+#  updated_at :datetime         not null               # 更新日時
 #
 
 class Category < ActiveRecord::Base
