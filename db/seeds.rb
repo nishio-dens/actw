1f35f70a712ac04a0f01f5fb117066e86b65c359
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 01:30:58 +0900

    @@ignore add

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+connection = ActiveRecord::Base.connection
+
+tables = %w[categories]
+
+tables.each do |table|
+  Rails.logger.log("TRUNCATE TABLE #{table}")
+  connection.execute("TRUNCATE #{table}")
+end
+
+Dir.glob("db/seed_sql/*.sql").each do |sql_file|
+  Rails.logger.log("load data #{sql_file}")
+  sql = File.read(sql_file)
+  statements = sql.split(/;$/)
+  statements.pop
+
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
+  end
+end
