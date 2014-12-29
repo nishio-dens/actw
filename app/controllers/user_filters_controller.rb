fcce8080ca376df772502497563b9c0a07cc2c8c
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 00:04:58 +0900

    @@ignore add

--- app/controllers/user_filters_controller.rb
+++ app/controllers/user_filters_controller.rb
@@ -1,4 +1,13 @@
 class UserFiltersController < ApplicationController
   def show
+    @user = User.find(params[:user_id])
+    @filter = Filter.find_by(user_id: @user.id, id: params[:id])
+    @products = Product
+      .joins(:product_filters)
+      .where(product_filters: { filter_id: @filter.id })
+      .order(published_at: :desc)
+      .distinct
+      .page(params[:page])
+      .decorate
   end
 end
