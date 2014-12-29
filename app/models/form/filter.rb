abe7d0b2f8be3479119cfed8d537286d794fe9ec
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-29 18:49:59 +0900

    @@ignore add

--- app/models/form/filter.rb
+++ app/models/form/filter.rb
@@ -6,6 +6,7 @@
 #  name           :string(255)      not null               # フィルタ名
 #  display_order  :integer          default("1"), not null # 表示順
 #  display_mypage :boolean          default("1"), not null # マイページ表示
+#  data_count     :integer          default("0"), not null # データ数
 #  user_id        :integer          not null               # ユーザID
 #  created_at     :datetime         not null               # 作成日時
 #  updated_at     :datetime         not null               # 更新日時
