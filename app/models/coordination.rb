16abd140bd9bb4488594baabc4fe4ccc6312fd46
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 16:06:20 +0900

    @@ignore add

--- app/models/coordination.rb
+++ app/models/coordination.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: coordinations # 自動連携
+#
+#  id                   :integer          not null, primary key
+#  user_id              :integer          not null              # ユーザID
+#  title                :string(255)      not null              # 連携名
+#  url                  :text(65535)                            # 連携先URL
+#  coordination_type_id :integer          not null              # 連携先タイプ
+#  created_at           :datetime         not null              # 作成日時
+#  updated_at           :datetime         not null              # 更新日時
+#
+
 class Coordination < ActiveRecord::Base
 end
