09639188f8ce7d11ef04682920a2ef0f91e5c0c3
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 17:19:04 +0900

    @@ignore add

--- app/controllers/auto_registrations_controller.rb
+++ app/controllers/auto_registrations_controller.rb
@@ -2,5 +2,6 @@ class AutoRegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @coordinations = Coordination.where(user_id: current_user.id)
   end
 end
