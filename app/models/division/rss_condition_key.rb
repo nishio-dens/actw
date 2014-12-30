127e34a520a2e49ff1eddecec266c5a440bf3300
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-31 01:37:55 +0900

    @@ignore add

--- app/models/division/rss_condition_key.rb
+++ app/models/division/rss_condition_key.rb
@@ -1,2 +1,11 @@
-class Division::RssConditionKey
+class Division::RssConditionKey < ActiveHash::Base
+  include ActiveHash::Enum
+
+  self.data = [
+    { id: 'title'        , name: 'タイトル' },
+    { id: 'author_name'  , name: '著者名' },
+    { id: 'tag'          , name: 'タグ' }
+  ]
+
+  enum_accessor :id
 end
