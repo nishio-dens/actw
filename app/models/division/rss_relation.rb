262669f20be78b7a4a857908d3dccacd2d7a2b9c
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-31 01:45:28 +0900

    @@ignore add

---
+++ app/models/division/rss_relation.rb
@@ -0,0 +1,10 @@
+class Division::RssRelation < ActiveHash::Base
+  include ActiveHash::Enum
+
+  self.data = [
+    { id: 'cont'      , name: '含む' },
+    { id: 'not_cont'  , name: '含まない' },
+  ]
+
+  enum_accessor :id
+end
