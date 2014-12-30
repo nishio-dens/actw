35b5997a779b0cafc0c77bd6773f9cfc327125cf
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-31 01:06:42 +0900

    @@ignore add

--- app/models/coordination_filter.rb
+++ app/models/coordination_filter.rb
@@ -1,12 +1,12 @@
 # == Schema Information
 #
-# Table name: coordination_condition_filters # 自動連携条件登録先フィルタ
+# Table name: coordination_filters # 自動連携登録先フィルタ
 #
-#  id                        :integer          not null, primary key
-#  coordination_condition_id :integer          not null              # 自動連携条件ID
-#  filter_id                 :integer          not null              # フィルタID
-#  created_at                :datetime         not null              # 作成日時
-#  updated_at                :datetime         not null              # 更新日時
+#  id              :integer          not null, primary key
+#  coordination_id :integer          not null              # 自動連携ID
+#  filter_id       :integer          not null              # フィルタID
+#  created_at      :datetime         not null              # 作成日時
+#  updated_at      :datetime         not null              # 更新日時
 #
 
 class CoordinationFilter < ActiveRecord::Base
