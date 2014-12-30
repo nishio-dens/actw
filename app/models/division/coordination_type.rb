e1b11366b17fc52227584cdbc9bf01121888906a
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 16:06:51 +0900

    @@ignore add

--- app/models/division/coordination_type.rb
+++ app/models/division/coordination_type.rb
@@ -1,2 +1,10 @@
-class CoordinationType < ActiveRecord::Base
+# == Schema Information
+#
+# Table name: division_coordination_types # 連携先タイプ
+#
+#  id   :integer          not null, primary key
+#  name :string(255)      not null              # 連携先タイプ
+#
+
+class Division::CoordinationType < ActiveRecord::Base
 end
