b6f65486ffc301f76952474ef86b98a0684639c0
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 20:55:59 +0900

    @@ignore add

--- app/models/division/coordination_type.rb
+++ app/models/division/coordination_type.rb
@@ -2,8 +2,9 @@
 #
 # Table name: division_coordination_types # 連携先タイプ
 #
-#  id   :integer          not null, primary key
-#  name :string(255)      not null              # 連携先タイプ
+#  id            :integer          not null, primary key
+#  name          :string(255)      not null              # 連携先タイプ
+#  division_type :string(10)       not null              # 区分値
 #
 
 class Division::CoordinationType < ActiveRecord::Base
--- app/models/form/rss.rb
+++ app/models/form/rss.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: coordinations # 自動連携
+#
+#  id                   :integer          not null, primary key
+#  user_id              :integer          not null              # ユーザID
+#  title                :string(255)      not null              # 連携名
+#  url                  :text(65535)                            # 連携先URL
+#  coordination_type_id :integer          not null              # 連携先タイプ
+#  last_updated_at      :datetime                               # 最終連携日時
+#  created_at           :datetime         not null              # 作成日時
+#  updated_at           :datetime         not null              # 更新日時
+#
+
 class Form::Rss < Coordination
   PERMITTED_ATTRIBUTES = %i(
     title url
