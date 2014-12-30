2f790d8ffdf26ee20266b82eca8db21e8c51c17e
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 16:44:49 +0900

    @@ignore add

--- app/models/coordination.rb
+++ app/models/coordination.rb
@@ -7,6 +7,7 @@
 #  title                :string(255)      not null              # 連携名
 #  url                  :text(65535)                            # 連携先URL
 #  coordination_type_id :integer          not null              # 連携先タイプ
+#  last_updated_at      :datetime                               # 最終連携日時
 #  created_at           :datetime         not null              # 作成日時
 #  updated_at           :datetime         not null              # 更新日時
 #
