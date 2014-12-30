32f593f1eecf55b886389461c88e597596ec2b64
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-30 20:52:00 +0900

    @@ignore add

--- db/seed_sql/division_coordination_type.sql
+++ db/seed_sql/division_coordination_type.sql
@@ -1,11 +1,11 @@
 SET FOREIGN_KEY_CHECKS=0;
 BEGIN;
 DELETE FROM `division_coordination_types`;
-INSERT INTO `division_coordination_types` (`id`, `name`)
+INSERT INTO `division_coordination_types` (`id`, `name`, `division_type`)
 VALUES
-  (1, 'RSS'),
-  (2, 'Github'),
-  (3, 'Qiita')
+(1, 'RSS', 'rss'),
+(2, 'Github', 'github'),
+(3, 'Qiita', 'qiita')
 ;
 COMMIT;
 SET FOREIGN_KEY_CHECKS=1;
