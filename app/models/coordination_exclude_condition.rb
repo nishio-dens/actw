24c650631e508f00e98f0a3be63eeb0e84a4a49f
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-10 18:28:54 +0900

    @@ignore add

---
+++ app/models/coordination_exclude_condition.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: coordination_exclude_conditions # 自動連携除外条件
+#
+#  id              :integer          not null, primary key
+#  coordination_id :integer          not null              # 自動連携ID
+#  condition_key   :string(50)       not null              # 連携条件キー
+#  predicate       :string(10)       not null              # 検索述語
+#  condition_value :string(255)      not null              # 条件値
+#  created_at      :datetime         not null              # 作成日時
+#  updated_at      :datetime         not null              # 更新日時
+#
+
+class CoordinationExcludeCondition < ActiveRecord::Base
+  belongs_to :coordination
+  belongs_to :category
+end
