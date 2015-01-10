68967c4b365bf4762694b7acc8ca9450af048def
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-11 02:45:16 +0900

    @@ignore add

---
+++ app/models/auto_coordination_job_history.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: auto_coordination_job_histories # 自動連携ジョブ履歴
+#
+#  id              :integer          not null, primary key
+#  coordination_id :integer          not null              # 自動連携ID
+#  job_id          :string(40)       not null              # ジョブID
+#  job_status_id   :integer          not null              # ジョブステータス
+#  program_id      :string(10)                             # 実行プログラム
+#  finished_at     :datetime                               # ジョブ実行完了日時
+#  created_at      :datetime         not null              # 作成日時
+#  updated_at      :datetime         not null              # 更新日時
+#
+
+class AutoCoordinationJobHistory < ActiveRecord::Base
+  belongs_to :coordination
+end
