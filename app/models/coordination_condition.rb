c8b224fb6a82719fe627a49783b68c0a93e0c338
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-10 18:26:00 +0900

    @@ignore add

--- app/models/coordination_condition.rb
+++ app/models/coordination_condition.rb
@@ -5,8 +5,8 @@
 #  id              :integer          not null, primary key
 #  coordination_id :integer          not null              # 自動連携ID
 #  condition_key   :string(50)       not null              # 連携条件キー
-#  relation        :string(10)       not null              # 比較演算
-#  condition_value :string(255)      not null              # 連携条件値
+#  predicate       :string(10)       not null              # 検索述語
+#  condition_value :string(255)      not null              # 条件値
 #  created_at      :datetime         not null              # 作成日時
 #  updated_at      :datetime         not null              # 更新日時
 #
