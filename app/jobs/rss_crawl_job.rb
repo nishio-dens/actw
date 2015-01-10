b33db95687577f6b3ae0ed19fec824aa6157c78f
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-11 02:53:08 +0900

    @@ignore add

--- app/jobs/rss_crawl_job.rb
+++ app/jobs/rss_crawl_job.rb
@@ -2,6 +2,11 @@ class RssCrawlJob < ActiveJob::Base
   queue_as :default
 
   def perform(coordination)
+    AutoCoordinationJobHistory.create(
+      job_id: self.job_id,
+      coordination_id: coordination.id,
+      job_status_id: 'start'
+    )
     Rails.logger.info("call #{coordination.id}")
   end
 end
