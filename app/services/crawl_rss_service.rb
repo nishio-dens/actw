64c0adfe25f04f815d479102511e256b7b42b34c
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-13 01:54:35 +0900

    @@ignore add

--- app/services/crawl_rss_service.rb
+++ app/services/crawl_rss_service.rb
@@ -1,6 +1,27 @@
 require 'feedjira'
 
 class CrawlRssService < BaseService
-  def execute
+  def execute(coordination)
+    feed = Feedjira::Feed.fetch_and_parse(coordination.url)
+    # TODO conditionを考慮
+    feed.entries.each do |entry|
+      persist_entry(coordination, entry)
+    end
+  end
+
+  private
+
+  def persist_entry(coordination, entry)
+    unless Product.find_by(user_id: coordination.user_id, url: entry.url).exists?
+      Product.create(
+        title: entry.title,
+        url: entry.url,
+        description: entry.summary,
+        category_id: coordination.category_id,
+        user_id: coordination.user_id,
+        manual: false,
+        published_at: entry.published
+      )
+    end
   end
 end
