cc20ce01b3bc47108a1bfc5e99921dd295e0ee7d
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-18 02:59:42 +0900

    @@ignore add

--- app/services/crawl_rss_service.rb
+++ app/services/crawl_rss_service.rb
@@ -12,7 +12,6 @@ class CrawlRssService < BaseService
   private
 
   def persist_entry(coordination, job_number, entry)
-    # TODO フィルタ追加・タグ追加
     unless Product.where(user_id: coordination.user_id, url: entry.url).exists?
       product = Product.new(
         title: entry.title,
@@ -24,6 +23,15 @@ class CrawlRssService < BaseService
         job_number: job_number,
         published_at: entry.published
       )
+      product.tag_list = entry.categories if coordination.tagging
+      coordination.coordination_filters.each do |filter|
+        product.product_filters.build(filter_id: filter.filter_id)
+      end
+      unless product.save
+        raise ActiveRecord::RecordInvalid(
+          "SaveError[coordination_id: #{coordination.id}]" + product.errors.full_messages.join("\n")
+        )
+      end
     end
   end
 end
