392615436b9e6acff63cac9cb938dff3cf23be80
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-18 19:42:22 +0900

    @@ignore add

--- app/models/auto_coordination_job_history.rb
+++ app/models/auto_coordination_job_history.rb
@@ -3,14 +3,16 @@
 # Table name: auto_coordination_job_histories # 自動連携ジョブ履歴
 #
 #  id                 :integer          not null, primary key
-#  coordination_id    :integer          not null              # 自動連携ID
-#  job_id             :string(40)       not null              # ジョブID
-#  job_status_id      :string(10)       not null              # ジョブステータス
-#  request_program_id :string(10)                             # 要求元プログラム
-#  error_message      :text(65535)                            # 実行エラーメッセージ
-#  finished_at        :datetime                               # ジョブ実行完了日時
-#  created_at         :datetime         not null              # 作成日時
-#  updated_at         :datetime         not null              # 更新日時
+#  coordination_id    :integer          not null               # 自動連携ID
+#  job_name           :string(255)      not null               # ジョブ名称
+#  job_id             :string(40)       not null               # ジョブID
+#  job_status_id      :string(10)       not null               # ジョブステータス
+#  data_count         :integer          default("0"), not null # 連携データ数
+#  request_program_id :string(10)                              # 要求元プログラム
+#  error_message      :text(65535)                             # 実行エラーメッセージ
+#  finished_at        :datetime                                # ジョブ実行完了日時
+#  created_at         :datetime         not null               # 作成日時
+#  updated_at         :datetime         not null               # 更新日時
 #
 
 class AutoCoordinationJobHistory < ActiveRecord::Base
