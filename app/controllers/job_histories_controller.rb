f1547118c2b41148236c1a9fb6a4e02d96728519
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-19 01:38:37 +0900

    @@ignore add

--- app/controllers/job_histories_controller.rb
+++ app/controllers/job_histories_controller.rb
@@ -1,5 +1,8 @@
 class JobHistoriesController < ApplicationController
   def show
+    @job_history = AutoCoordinationJobHistory
+      .find_by!(user_id: current_user.id, id: params[:id])
+    @products = Product.where(job_number: @job_history.id).decorate
   end
 
   def destroy
