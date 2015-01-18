d8d0202951481a63e046f51f2ade1eee67d94a33
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-18 19:57:45 +0900

    @@ignore add

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -9,8 +9,8 @@
 #  tagger_id     :integer                                # タグ所有者ID
 #  tagger_type   :string(255)                            # タグ所有者クラス
 #  context       :string(128)                            # タグ種別
-#  created_at    :datetime         not null              # 作成日時
-#  updated_at    :datetime         not null              # 更新日時
+#  created_at    :datetime                               # 作成日時
+#  updated_at    :datetime                               # 更新日時
 #
 
 class Tagging < ActiveRecord::Base
